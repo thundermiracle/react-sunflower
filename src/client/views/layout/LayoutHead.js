ca375c0eb7bc67f08f483d8843ac702b5d6b68dc
thundermiracle <cissoliu@gmail.com>
2018-05-18 20:29:23 +0900

    apply gtag to every page

--- src/client/views/layout/LayoutHead.js
+++ src/client/views/layout/LayoutHead.js
@@ -1,12 +1,26 @@
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 
+const config = {
+  google: 'UA-119418003-1',
+};
+
 const LayoutHead = (props) => {
   const { title } = props;
   return (
     <Head>
-      <title>{ title }</title>
+      <title>{ title.toUpperCase() || 'TOP' }-- Draw Sunflower</title>
       <meta charSet="utf-8" />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${config.google}`} />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments)};
+gtag('js', new Date());gtag('config', '${config.google}');
+              `,
+        }}
+      />
     </Head>
   );
 };
