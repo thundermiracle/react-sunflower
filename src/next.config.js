37e8fdc8a4f3c5494ae7bad7e02dd4e22a141c37
daniel <daniel@thundermiracle.com>
2019-03-07 21:59:01 +0900

    fix lodash version warning

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,6 @@
 const config = require("../config/client");
 const PATH_MAP = require("./client/PathMap");
 const { EnvironmentPlugin } = require('webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 process.env.PKG_VERSION = process.env.npm_package_version;
 process.env.PKG_NAME = process.env.npm_package_name;
@@ -13,7 +12,6 @@ module.exports = {
   assetPrefix: process.env.SUBFOLDER,
   webpack: (config, { dev }) => {
     config.plugins.push(new EnvironmentPlugin(['PKG_VERSION', 'PKG_NAME', 'SUBFOLDER']));
-    config.plugins.push(new CompressionPlugin());
     return config;
   },
   distDir: '../.next',
