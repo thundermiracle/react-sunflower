199007c4e05c95925d8b41ded74c0d7b0baf295c
thundermiracle <cissoliu@gmail.com>
2019-03-09 11:47:19 +0900

    versionup next-mui-helper to speedup the site

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,7 @@
 const config = require("../config/client");
 const PATH_MAP = require("./client/PathMap");
-const makeNextExportPathMap = require('next-mui-helper/util/makeNextExportPathMap');
 const { EnvironmentPlugin } = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 process.env.PKG_VERSION = process.env.npm_package_version;
 process.env.PKG_NAME = process.env.npm_package_name;
@@ -10,10 +10,10 @@ process.env.PKG_NAME = process.env.npm_package_name;
 process.env.SUBFOLDER = (config.subfolder && process.env.NODE_ENV === 'production') ? `/${process.env.npm_package_name}` : '';
 
 module.exports = {
-  exportPathMap: () => makeNextExportPathMap(PATH_MAP),
   assetPrefix: process.env.SUBFOLDER,
   webpack: (config, { dev }) => {
     config.plugins.push(new EnvironmentPlugin(['PKG_VERSION', 'PKG_NAME', 'SUBFOLDER']));
+    config.plugins.push(new CompressionPlugin());
     return config;
   },
   distDir: '../.next',
