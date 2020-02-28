9a73e2a52f186ea39a9421251e2cc343b97e1fbf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 12:32:36 +0000

    Deploy thundermiracle/react-sunflower to github.com/thundermiracle/react-sunflower.git:gh-pages

---
+++ _next/static/nuia6XUj7yj7wPdrtQdlZ/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{539:function(e,n,t){__NEXT_REGISTER_PAGE("/",function(){return e.exports=t(540),{page:e.exports.default}})},540:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o);n.default=function(){return r.a.createElement("div",null,"Select a shape from menu to draw.")}}},[[539,1,0]]]);
